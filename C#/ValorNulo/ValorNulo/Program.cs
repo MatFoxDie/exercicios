5eafa30f4bb2f3ce2123e763cc979d73dd335cf4
Matheus Soares Camacho <55390725+MatFoxDie@users.noreply.github.com>
2021-04-11 17:08:53 -0300

    Add files via upload

---
+++ C#/ValorNulo/ValorNulo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValorNulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double? x = null;
+            double? y = 10.0;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(x.HasValue);
+
+            if(x.HasValue)
+                Console.WriteLine(x.Value);
+            else
+                Console.WriteLine("X is null");
+
+            if (y.HasValue)
+                Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("Y is null");
+
+            double z = x ?? 1.0;
+
+            Console.WriteLine(z);
+
+            Console.ReadKey();
+        }
+    }
+}
