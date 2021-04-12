c9937ffe3f831f6baa3930a94b02ae077865abc7
Matheus Soares Camacho <55390725+MatFoxDie@users.noreply.github.com>
2021-04-11 23:36:04 -0300

    Add files via upload

---
+++ C#/EnumsProject/EnumsProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using Enum.Entities;
+using Enum.Entities.Enums;
+using System;
+
+
+namespace Enum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PedingPayment
+            };
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PedingPayment.ToString();
+
+            Console.WriteLine(txt);
+
+            
+            Console.ReadKey();
+        }
+    }
+}
